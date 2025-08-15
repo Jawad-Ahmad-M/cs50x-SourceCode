fd299cd4732cc5978cb553f5178d5dfe553ba591
Jawad-Ahmad-M <jdmgl2238@gmail.com>
2025-08-15 20:55:51 +0500

    Added Sentimental Hello Problem

---
+++ Week 6/sentimental-hello/hello.py
@@ -0,0 +1,5 @@
+# Give the prompt to the user to enter his name after the string present in the double Quotes
+name = input("What is your name? ")
+
+print("Hello", name)  # Prints Hello and name with a space automatically.
+print("Hello " + name)  # Prints Hello name but now we have to enter the space manually
